7653ee4192d8f2e39a4780dfccc652407c4fe9eb
Claudia Chittim <cchittim10101@gmail.com>
2022-05-08 20:05:53 -0500

    starter code for veest-news-crunch

---
+++ config/env.sql
@@ -0,0 +1,3 @@
+DB_NAME='veest_news_crunch_db'
+DB_USER='your-mysql-username'
+DB_PW='your-mysql-password'
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS veest_news_crunch_db;
+
+CREATE DATABASE veest_news_crunch_db;
